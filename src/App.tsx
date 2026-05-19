c43c9a7e0cd571160f9eac664aee14e4389a3be4
zhaolong <1679468108@qq.com>
2026-05-19 10:44:20 +0800

    改为hash路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Sidebar } from './components/Sidebar/Sidebar'
 import { routes } from './routes'
